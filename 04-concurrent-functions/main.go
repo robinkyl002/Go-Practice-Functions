cc8fb8cb2745adc15cb6958fe2eb78a7311fb357
Kyle Robinson <ky.robinson72@gmail.com>
2024-03-12 15:56:26 -0600

    Added code from the previous lesson to modify

--- 04-concurrent-functions/main.go
+++ 04-concurrent-functions/main.go
@@ -3,7 +3,10 @@
 
 package main
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 // Change these boolean values to control whether you see 
 // the expected answer and/or hints.
@@ -15,7 +18,28 @@ func CalculateVariance(numbers []int) (*float64, error) {
 	if numbers == nil || len(numbers) == 0{
 		return nil, errors.New("not implemented")
 	} else {
-		return nil, nil
+		
+		n := float64(len(numbers))
+		sum := func(f func(int) float64) float64{
+			var s float64
+			for _, num := range numbers {
+				s += f(num)
+			}
+			
+			return s
+		}
+
+		mean := sum(func(i int) float64 {
+			return float64(i)
+		}) / n
+
+		sumRes := sum(func(i int) float64{
+			return math.Pow(float64(i) - mean, 2)
+		})
+		
+		variance := sumRes / n
+		
+		return &variance, nil
 	}
 	
 }
