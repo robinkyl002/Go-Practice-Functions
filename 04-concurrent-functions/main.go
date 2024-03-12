b522090b9b918859e2a6c8cbbdb44ddbd98664d7
Kyle Robinson <ky.robinson72@gmail.com>
2024-03-12 15:54:37 -0600

    Added if-else to handle issues with empty slices

--- 04-concurrent-functions/main.go
+++ 04-concurrent-functions/main.go
@@ -12,5 +12,10 @@ const showHints = false;
 
 // CalculateVariance returns variance of the numbers slice, or an error.
 func CalculateVariance(numbers []int) (*float64, error) {
-	return nil, errors.New("not implemented")
+	if numbers == nil || len(numbers) == 0{
+		return nil, errors.New("not implemented")
+	} else {
+		return nil, nil
+	}
+	
 }
