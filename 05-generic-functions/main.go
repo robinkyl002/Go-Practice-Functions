8dc5fd5e27c898fa96b90ee8bc51b8aeb1632220
Kyle Robinson <ky.robinson72@gmail.com>
2024-03-12 16:29:52 -0600

    Created generic sum function and used it to calculate the mean as type float64

--- 05-generic-functions/main.go
+++ 05-generic-functions/main.go
@@ -3,7 +3,10 @@
 
 package main
 
-import "errors"
+import (
+	"errors"
+	// "constraints"
+)
 
 // Change these boolean values to control whether you see
 // the expected answer and/or hints.
@@ -16,7 +19,18 @@ func CalculateMean[T any](numbers []T) (*float64, error) {
 	if numbers == nil || len(numbers) == 0{
 		return nil, errors.New("empty set")	
 	} else {
-		return nil, nil
+		var mean float64
+		n := float64(len(numbers))
+		
+		mean = float64(Sum(numbers)) / n
+		return &mean, nil
 	}
-    
+}
+
+func Sum[T int | uint | float64](x []T) T {
+	var sum T
+	for _, num := range x {
+		sum += num
+	}
+	return sum
 }
