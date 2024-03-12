7c59a9137db83fe5466b738fd0d7a358d641d317
Kyle Robinson <ky.robinson72@gmail.com>
2024-03-12 15:53:19 -0600

    Created the file and added the boilerplate code

---
+++ 04-concurrent-functions/main.go
@@ -0,0 +1,16 @@
+// Write your answer here, and then test your code.
+// Your job is to implement the CalculateVariance() method.
+
+package main
+
+import "errors"
+
+// Change these boolean values to control whether you see 
+// the expected answer and/or hints.
+const showExpectedResult = false;
+const showHints = false;
+
+// CalculateVariance returns variance of the numbers slice, or an error.
+func CalculateVariance(numbers []int) (*float64, error) {
+	return nil, errors.New("not implemented")
+}
